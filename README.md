3c1b3f9ab8683d8313691e0b620c4c172d857997
Allen Rodriguez <A.Rodriguez@allens-mbp.lan>
2020-03-08 18:08:41 -0700

    testing README

--- README.md
+++ README.md
@@ -4,4 +4,35 @@ EE P 520: Final Project
 Software Engineering For Embedded Applications
 ---
 
-This repository contains the full implementation of the final project developed for the EEP520 course, Software Engineering for Embedded Applications. 
+This repository contains the full implementation of the final project developed for the EEP520 course, Software Engineering for Embedded Applications. The contents of this repository are: (1) the SimpleMaze project which, using ELMA and ENVIRO creates a robot that navigates through a maze, and (2) XXX which also makes use of ELMA and ENVIRO and XXX. Please note that each of the folders on this repository hold individual README files that further expand on the project implementation, use, and development. 
+
+Installation
+---
+
+As stated, both projects make use of ELMA and ENVIRO. Both repositories and information on how to use these can be found in the links displayed below. 
+
+Repository and further information on both can be found in the following links:
+
+[ELMA](https://github.com/klavinslab/elma)<br />
+[ENVIRO](https://github.com/klavinslab/enviro) <br />
+
+Once this full repository has been downloaded, make sure to have [Docker](https://www.docker.com) installed in your computer. Both elma and enviro have been preloaded to a Docker image which makes calling these packages easier. More information can be found in ELMA and ENVIRO's repo. Once Docker is installed simply do: <br />
+```bash
+docker run -p80:80 -p8765:8765 -v $PWD:/source - it klavins/enviro:v1.1 bash
+```
+*Please note that at the time this file was created, ENVIRO's latest version was v1.1. Please refer to ENVIRO's repository for more information on the latest version and updates.
+
+Usage
+---
+
+Please refer to each project's individual folder for more information on how to run the code and see the robot simulation. See the details on the maze simulation in the `SimpleMaze` folder and the details for the XXX simulation in the `XXX` folder. 
+
+Contributing
+---
+
+This project was completed as part of the final requirements for the completion of EEP520 course. At this point, the project is not open for external contributions.
+
+License
+---
+
+All contents of this repository are open source and uses the MIT license. 
---
+++ SimpleMaze/src/my_robot.h
@@ -0,0 +1,98 @@
+#ifndef __MY_ROBOT_AGENT__H
+#define __MY_ROBOT_AGENT__H 
+
+#include "enviro.h"
+#include <iostream>
+#include <math.h>
+
+namespace{
+    using namespace enviro;
+    using namespace std;
+
+    int globalVarR = 0;
+    int globarVarL = 0;
+
+    class Forward : public State, public AgentInterface {
+        public:
+        void entry(const Event& e) {}
+        void during() {
+            track_velocity(10,0);
+            if ( sensor_value(0) < 30 || sensor_value(1) < 10 || sensor_value(2) < 10 ) {
+                emit(Event(tin));
+            }
+        }
+        void exit(const Event& e) {}
+        void set_tin_name(std::string s) {tin = s;}
+        string tin;
+    };
+
+    class Angular : public State, public AgentInterface {
+        public:
+        void entry(const Event& e) {
+            rateR = 1.0;
+            rateL = -1.0;
+        }
+        void during() {
+            if (sensor_value(1) > sensor_value(2)) {
+                track_velocity(0,rateR);
+                if (sensor_value(0) > 60) {
+                    emit(Event(tin));
+                }
+            } else if (sensor_value(1) == sensor_value(2)) {
+                if (sensor_value(3) > sensor_value(4)) {
+                    track_velocity(0, rateR);
+                    if (sensor_value(0) > 60) {
+                        emit(Event(tin));
+                    }
+                } else {
+                    track_velocity(0,rateL);
+                    if (sensor_value(0) > 60) {
+                        emit(Event(tin));
+                    }
+                }
+            } else {
+                track_velocity(0,rateL);
+                if (sensor_value(0) > 60) {
+                    emit(Event(tin));
+                }
+            }
+        }
+        void exit(const Event& e) {}
+        double rateR;
+        double rateL;
+        void set_tin_name(std::string s) { tin = s; }
+        std::string tin;
+    };
+
+    class MyRobotController : public StateMachine, public AgentInterface {
+
+    public:
+    MyRobotController() : StateMachine() {
+        set_initial(mForward);
+        theEvent = "tin_" + std::to_string(rand()%1000);
+        add_transition(theEvent, mForward, angled);
+        add_transition(theEvent, angled, mForward);
+        mForward.set_tin_name(theEvent);
+        angled.set_tin_name(theEvent);
+    }
+    
+    Forward mForward;
+    Angular angled;
+    string theEvent;
+    
+    };
+    
+    class MyRobot : public Agent {
+        public:
+        MyRobot(json spec, World& world) : Agent(spec, world) {
+        add_process(c);
+        
+        }
+        
+        MyRobotController c;
+    };
+    
+    DECLARE_INTERFACE(MyRobot)
+
+}
+#endif
